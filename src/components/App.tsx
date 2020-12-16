55553a6f21c9e307fd3b2e4e45bec2004ee5b87c
meddyrainzo <meddyakpan@outlook.com>
2020-12-16 12:28:26 +0100

    Added quote list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,34 +1,11 @@
 import React, { FC } from 'react';
-import Quote from './quote/Quote';
 import './App.scss';
-
-const firstQuote = {
-  id: '123345',
-  quote:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting \
-  industry. Lorem Ipsum has been the industry's standard dummy text ever \
-  since the 1500s",
-  author: 'Authur',
-  postedBy: { firstname: 'First', lastname: 'Last' },
-  postedOn: '20th Dec 2020',
-  numberOfComments: 10,
-  numberOfLikes: 15,
-  likedByYou: false,
-};
+import QuoteList from './quote/QuoteList';
 
 const App: FC = () => {
   return (
     <div>
-      <Quote
-        id={firstQuote.id}
-        quote={firstQuote.quote}
-        author={firstQuote.author}
-        postedBy={firstQuote.postedBy}
-        postedOn={firstQuote.postedOn}
-        numberOfComments={firstQuote.numberOfComments}
-        numberOfLikes={firstQuote.numberOfLikes}
-        likedByYou={firstQuote.likedByYou}
-      />
+      <QuoteList />
     </div>
   );
 };
--- src/components/quote/QuoteList.tsx
+++ src/components/quote/QuoteList.tsx
@@ -1,1 +1,32 @@
-import React from 'react'
+import React, { FC, useEffect, useState } from 'react';
+
+import sampleQuotes from '../../fake-server/sampleQuotes';
+import { SingleQuote } from './models/singleQuote';
+import Quote from './Quote';
+import './QuoteList.scss';
+
+const QuoteList: FC = () => {
+  const [quotes, setQuotes] = useState(Array<SingleQuote>());
+  useEffect(() => {
+    setQuotes(sampleQuotes);
+  }, []);
+
+  const createQuotes = () =>
+    quotes.map((quote) => (
+      <Quote
+        key={quote.id}
+        id={quote.id}
+        quote={quote.quote}
+        author={quote.author}
+        postedBy={quote.postedBy}
+        postedOn={quote.postedOn}
+        numberOfComments={quote.numberOfComments}
+        numberOfLikes={quote.numberOfLikes}
+        likedByYou={quote.likedByYou}
+      />
+    ));
+
+  return <div className='quote-list'>{createQuotes()}</div>;
+};
+
+export default QuoteList;
