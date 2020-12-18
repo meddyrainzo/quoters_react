b8e7bd5c86869a329f3261086ec39163308149e3
meddyrainzo <meddyakpan@outlook.com>
2020-12-18 12:22:51 +0100

    Added formik to login

--- src/components/identity/Login.tsx
+++ src/components/identity/Login.tsx
@@ -1,87 +1,62 @@
-import React, { ChangeEvent, FocusEvent, FC, useState } from 'react';
+import React, { FC } from 'react';
 import { useDispatch } from 'react-redux';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 import { loginUserAction } from '../../actions/loginActions';
 import { LoginRequest } from '../../models/requests/loginRequest';
-import { emailValidator, wordValidator } from '../../utils/verifiers/verifier';
 
 import './Identity.scss';
+import { FormErrorMessages } from '../form/formErrorMessages';
+import TextField from '../form/TextField';
 
-let initialRequest: LoginRequest = {
-  email: '',
-  password: '',
-};
-
-let initialErrors = {
+let request: LoginRequest = {
   email: '',
   password: '',
 };
 
 const Login: FC = () => {
-  const [request, setRequest] = useState(initialRequest);
-  const [errors, setErrors] = useState(initialErrors);
   const dispatch = useDispatch();
-
-  const { email, password } = request;
-
-  const onValueChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setRequest({ ...request, [name]: value });
-  };
-
-  const validateEmail = (event: FocusEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setErrors({ ...errors, [name]: emailValidator(value) });
-  };
-
-  const validatePassword = (event: FocusEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setErrors({ ...errors, [name]: wordValidator(name, value) });
-  };
 
   return (
     <div className='form-page'>
       <div className='identity-title'>Sign in</div>
-      <div className='form'>
-        <label htmlFor='email' className='input-label'>
-          Email *
-        </label>
-        <input
-          type='email'
-          name='email'
-          value={email}
-          onChange={onValueChange}
-          className='text-field'
-          onBlur={validateEmail}
-          placeholder='Please enter your email'
-        />
-        {errors.email && <span className='error-label'>{errors.email}</span>}
-        <label htmlFor='email' className='input-label'>
-          Password *
-        </label>
-        <input
-          type='password'
-          name='password'
-          value={password}
-          onChange={onValueChange}
-          onBlur={validatePassword}
-          className='text-field'
-          placeholder='Please enter your password'
-        />
-        {errors.password && (
-          <span className='error-label'>{errors.password}</span>
+      <Formik
+        initialValues={request}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email(FormErrorMessages.EMAIL)
+            .required(FormErrorMessages.REQUIRED),
+          password: Yup.string().required(FormErrorMessages.REQUIRED),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          dispatch(loginUserAction(values));
+          setSubmitting(false);
+        }}
+      >
+        {({ isValid, dirty }) => (
+          <Form className='form'>
+            <TextField
+              name='email'
+              type='email'
+              placeholder='Ex. test@mail.com'
+              label='Email *'
+            />
+            <TextField
+              name='password'
+              type='password'
+              placeholder='Please enter your password'
+              label='Password *'
+            />
+            <button className='submit-button' disabled={!(isValid && dirty)}>
+              Sign in
+            </button>
+            <div>
+              Already have an account?
+              <button className='second-option'>Sign in</button>
+            </div>
+          </Form>
         )}
-        <button className='forgotten'>Forgotten password?</button>
-        <button
-          className='submit-button'
-          onClick={() => dispatch(loginUserAction(request))}
-        >
-          Sign in
-        </button>
-        <div>
-          Already have an account?
-          <button className='second-option'>Sign in</button>
-        </div>
-      </div>
+      </Formik>
     </div>
   );
 };
