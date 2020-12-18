2b97d8e7d55eb76d876befaf4b95fe67ac378d14
meddyrainzo <meddyakpan@outlook.com>
2020-12-18 13:12:38 +0100

    Changed the header menu based on if user is logged in or not

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,23 +1,43 @@
 import React, { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { UserState } from '../../reducers/loginReducer';
+import { nameAbbreviator } from '../../utils/nameInitialsCreator';
 import ProfileImage from '../profile/ProfileImage';
 
 import './Header.scss';
 
 const Header: FC = () => {
-  const initials = 'FL';
+  const currentUser = useSelector((state: UserState) => state.currentUser);
+
+  const createHeaderMenu = () => {
+    console.log(JSON.stringify(currentUser, null, 2));
+    if (currentUser.email) {
+      const initials = nameAbbreviator(
+        currentUser.firstname,
+        currentUser.lastname
+      );
+      return (
+        <>
+          <button className='add-quote-button'>Add quote</button>
+          <ProfileImage initials={initials} />
+          <button className='borderless-header-button'>Logout</button>
+        </>
+      );
+    }
+    return (
+      <>
+        <button className='borderless-header-button'>Register</button>
+        <button className='borderless-header-button'>Sign in</button>
+      </>
+    );
+  };
 
   return (
     <div className='header-container'>
       <div className='title'>
         <span>Quoters</span>
       </div>
-      <div className='header-menu'>
-        <button className='add-quote-button'>Add quote</button>
-        <ProfileImage initials={initials} />
-        <a href='#' className='menu-text'>
-          logout
-        </a>
-      </div>
+      <div className='header-menu'>{createHeaderMenu()}</div>
     </div>
   );
 };
